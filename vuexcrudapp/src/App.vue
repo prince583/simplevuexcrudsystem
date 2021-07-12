7877e179f86698e8967cd980fa84d727af2a850b
Prince <prince.saini@softobiz.com>
2021-07-12 09:15:16 +0530

    initial commit

---
+++ vuexcrudapp/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <h1>Todos</h1>
+    <h3>Completed:{{ $store.getters.completedTodos}}</h3>
+    <h3>Pending:</h3>
+    <TodosList/>
+    <Todoform/>
+  </div>
+</template>
+
+<script>
+import TodosList from './components/TodosList.vue'
+import Todoform from './components/Todoform.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TodosList,
+    Todoform
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vuexcrudapp/src/components/TodosList.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+    <todo v-for="(todo, index) in todos" :key="index" :todo="todo"/>
+    </div>
+
+</template>
+
+<script>
+import Todo from "./Todo";
+export default {
+    components: {
+        Todo
+    },
+
+    computed: {
+        todos() {
+            return this.$store.state.todos;
+        }
+        }
+    
+    
+};
+</script>
+
+<style>
+
+
+
+</style>
